8723f7b24d0f1c12dea33430b12c5cc47631a14b
feliciagutmans <fgutmans@gmail.com>
2017-12-09 18:02:26 +0000

    updating final version
    specified user input to not be “”
    Correcting comments
    Removing excess files

--- P+S_A3_3 copy.js
+++ P+S_A3_3 copy.js
@@ -4,21 +4,23 @@ by recording the name of each customer currently on the trampoline and whether
 they are an adult or a child. It must also make sure that no more than five
 people are on the trampoline at any one time. */
 
-/* Defines the max amount of customers allowed on trampoline at one time to be 5 */
+/* Defines max amount of customers allowed on trampoline at one time to be 5
+(max objects in array list = [0-4]) */
 const MAX_CUSTOMERS = 4;
 
+//Defines customer list as an array
 var customerList = new Array();
 
-/* Defines recurring error message for when no customers have been inputted*/
+// Defines recurring error message for when no customers have been inputted
 var inputError = 'Error- there are no customers to ';
 
-//Here we allow the user to input new customers
+//Allows the user to input new customers
 function addCustomer ()
 {
   /* If trampoline list is longer than 5 customers, prevent user from adding
   any more and return error message, prompting user to delete customers. */
   if (customerList.length > MAX_CUSTOMERS)
-    alert ('error- maxiumum customers reached. Please remove one customer');
+    alert ('error- maxiumum customers reached. Please remove one or more customers.');
   else
   {
   var newIndex = customerList.length;
@@ -26,13 +28,19 @@ function addCustomer ()
 
   //Asks user to input the customer's name
   customerList[newIndex].name = prompt('What is the customer\'s name?').trim();
+  //Makes sure user inputs name and not just blank space and prompts new input otherwise
+  while (customerList[newIndex].name=='')
+	  customerList[newIndex].name = prompt('Error - please enter a name:').trim();
   //Asks user to input whether the customer is an 'adult' or a 'child'
   customerList[newIndex].status = prompt('Are they an \'adult\' or a \'child\'?').trim();
+  //Makes sure user inputs right status type and prompts new input otherwise
+  while (!(customerList[newIndex].status== 'child' || customerList[newIndex].status == 'adult'))
+    customerList[newIndex].status = prompt('Error - enter \'child\' or \'adult\':');
   }
 }
 
 
-//Defines that all customers be displayed alongside their status type
+//Defines that all customers be displayed, alongside their status type
 function displayAllCustomers ()
 {
   var message = ''
@@ -48,7 +56,7 @@ function displayAllCustomers ()
   alert(message);
 }
 
-//Allowing user to delete the last customer inputted
+//Allows user to delete the last customer on the list, if customers have been inputted
 function deleteLastCustomer ()
 {
   if (customerList.length > 0)
@@ -57,53 +65,53 @@ function deleteLastCustomer ()
     alert('The customer has been deleted')
   }
   else
-  alert(inputError + String('delete.')); // Returns error if no customers to delete
+  alert(inputError + String('delete.')); // Returns error message if no customers to delete
 }
 
 //Distinguishes two customer types
 function displayCustomerType (statusType)
 {
-    var message = '' //when user inputs message
+    var message = '' //If there is previous user input, runs the for-loop
     for (var i = 0; i < customerList.length; i++)
     {
     if (customerList[i].status == statusType)
         message += customerList[i].name + ': ' + String(customerList[i].status) + '.\n';
     }
-    if (message=='')
+    if (message=='') //If no customers to display, returns error message
         message = (inputError + String('display.'));
     alert(message);
   }
 
-//Prompting user to input the name and status type of the customer they want to delete
+//Prompts user to input the name and status type of the customer they want to delete
 function identifyThenDeleteCustomer ()
 {
     var customerName = prompt('Enter the name of the customer to delete:').trim();
     var customerStatus = prompt('Enter \'child\' or \'adult\':').trim();
+//Prompts user to input specific status type, otherwise returns error message
     while (!(customerStatus == 'child' || customerStatus == 'adult'))
         customerStatus = prompt('Error - enter \'child\' or \'adult\':').trim();
     deleteCustomer(customerName,customerStatus);
 }
 
 
-// Prompting user to delete specific customer by name and status
+// Allows user to delete specific customer by name and status
 function deleteCustomer (aName, aStatus)
 {
   for (i=0;i < customerList.length;i++)
   {
-    //Finds the position of the person the user wants to delete
+//Tells function to run only if corresponding user name and status type inputted
     if (customerList[i].name==aName && customerList[i].status == aStatus)
     {
-      /* Keeps repeating until it gets to the second to last person in the
-       list*/
+// Keeps repeating until it gets to the second to last person in the list
       while(i < customerList.length - 1)
       {
-        /* moves the person who is one ahead of the current position
-         to the current position in the customer list */
+//Moves the person who is one ahead of the current position to the current position in the list
         customerList[i].name = customerList[i+1].name;
         customerList[i].status = customerList[i+1].status;
         i++
       }
-      // removes the duplicated last person in the list
+      /* removes the duplicated last person in the list and returns confirmation
+      message. If input is incorrect, leaves the list alone and returns to main page  */
       deleteLastCustomer()
     }
   }
